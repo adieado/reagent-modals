79041127c4177bd3a6193433faac3950b0459aab
Adi <adieado@gmail.com>
2017-07-18 16:21:45 +0300

    fix body scroll issue

--- src/reagent_modals/modals.cljs
+++ src/reagent_modals/modals.cljs
@@ -2,7 +2,8 @@
   (:require [reagent.core :as r :refer [atom]]
             [goog.dom :as dom]
             [goog.events :as events]
-            [goog.array :as g-array])
+            [goog.array :as g-array]
+            [goog.dom.classlist :as g-c])
   (:import [goog.events EventType]))
 
 ;;; Make sure to create the modal-window element somewhere in the dom.
@@ -39,7 +40,19 @@
       (set! (.-top (.-style (first (g-array/toArray (dom/getChildren m))))) y)
       (set! (.-left (.-style (first (g-array/toArray (dom/getChildren m))))) x)
       (set! (.-position (.-style (first (g-array/toArray (dom/getChildren m))))) "absolute")
-    )))
+      ))
+  (let [m (js/jQuery (get-modal))]
+    (.call (aget m "on") m "hidden.bs.modal"
+           (fn [d]
+             (let [bd (.-body js/document)]
+               (if (and (not (nil? x)) (not (nil? y)))
+                 (if-let [cl (.-classList bd)]
+                   (.add cl "modal-open")
+                   (set! (.-className bd) "modal-open"))
+                 (do
+                   (if-let [cl (.-classList bd)]
+                    (.remove cl "modal-open"))))
+               )))))
 
 (defn close-modal! []
   (let [m (js/jQuery (get-modal))]
@@ -48,10 +61,9 @@
 (defn close-button
   "A pre-configured close button. Just include it anywhere in the
    modal to let the user dismiss it." []
-   [:button.close {:type "button" :data-dismiss "modal"}
-     [:span.glyphicon.glyphicon-remove {:aria-hidden "true"}]
-     [:span.sr-only "Close"]])
-
+  [:button.close {:type "button" :data-dismiss "modal"}
+   [:span.glyphicon.glyphicon-remove {:aria-hidden "true"}]
+   [:span.sr-only "Close"]])
 
 
 (defn modal-window* [m-id]
@@ -64,20 +76,25 @@
        content]]]))
 
 (defn modal-window [m-id]
-  (with-meta
-    (modal-window* m-id)
-    {:component-did-mount
-     (fn [e] (let [m (js/jQuery (get-modal))]
-               (.call (aget m "on") m "hidden.bs.modal"
-                      #(do (when-let [f (:hidden (get @modal-content m-id))] (f))
-                           (swap! modal-content assoc m-id {:content [:div]}))) ;;clear the modal when hidden
-               (.call (aget m "on") m "shown.bs.modal"
-                      #(when-let [f (:shown (get @modal-content m-id))] (f)))
-               (.call (aget m "on") m "hide.bs.modal"
-                      #(when-let [f (:hide (get @modal-content m-id))] (do
-                                                                          (f)
-                                                                          (reset! @modal-content {})
-                                                                          (reset! @modal-id nil))))))}))
+  (r/create-class
+   {:reagent-render modal-window*
+    :component-did-mount (fn [component]
+                           (let [m (js/jQuery (get-modal))]
+                             (.call (aget m "on") m "hidden.bs.modal"
+                                    (fn [d]
+                                      (if (> (count @modal-content) 1)
+                                        (dom/add (dom/getElement "body") "modal-open")))) ;;clear the modal when hidden
+                             (.call (aget m "on") m "shown.bs.modal"
+                                    #(when-let [f (:shown (get @modal-content m-id))]
+                                       (f)))
+                             (.call (aget m "on") m "hide.bs.modal"
+                                    #(when-let [f (:hide (get @modal-content m-id))]
+                                       (f)
+                                       (if (> (count @modal-content) 1)
+                                         (dom/add (dom/getElement "body") "modal-open"))
+                                       (reset! @modal-content {})
+                                       (reset! @modal-id nil)
+                                       ))))}))
 
 
 ;;; main function
@@ -97,8 +114,8 @@
                   \"static\" : backdrop, but doesn't close the model when clicked upon.
                   false : no backdrop."
   ([m-id reagent-content]
-    (reset! modal-id m-id)
-    (modal! reagent-content nil))
+   (reset! modal-id m-id)
+   (modal! reagent-content nil))
   ([m-id reagent-content configs]
    (reset! modal-id m-id)
    (swap! modal-content assoc m-id (merge {:content reagent-content} configs))
